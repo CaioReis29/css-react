3dbad0492ccfda8ff9d102e0d641eb00aaa9be09
CaioReis29 <caio201612@gmail.com>
2023-04-15 18:43:35 -0300

    css-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import MyComponents from './components/MyComponents';
+import { useState } from 'react';
+import Title from './components/Title';
 
 function App() {
+
+  const n = 15;
+  const [name] = useState('Caio');
+
+  const redTitle = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* css global */}
+      <h1>Usando css no React</h1>
+      {/* css de componentes */}
+      <MyComponents/>
+      <p>Paragrafo do app.js</p>
+      <p className="my-comp-paragraph">Agora esse só vai funcionar no componente</p>
+      {/* inline css */}
+      <p style={{color: 'blue', padding: '25px', boderTop: '2px solid red'}}>Este texto está sendo estilizado de forma inline</p>
+      {/* css inline dinamico */}
+      <h2 style={n < 10 ? ({color: 'purple'}) : ({color: 'pink'})}>CSS dinamico</h2>
+      <h2 style={n > 10 ? ({color: 'purple'}) : ({color: 'pink'})}>CSS dinamico</h2>
+      <h2 style={name === 'Caio' ? ({color: 'green', backgroundColor: '#000'}) : null }>
+        Teste nome
+      </h2>
+      <h2 style={name !== 'Caio' ? ({color: 'green', backgroundColor: '#000'}) : null }>
+        Teste nome
+      </h2>
+      {/* classe dinamica */}
+      <h2 className={redTitle ? 'red-title' : 'title'}>Esse titulo vai ter classe dinamica</h2>
+      <h2 className={redTitle ? 'title' : 'red-title'}>Esses titulo vai ter classe dinamica</h2>
+      {/* CSS modules */}
+      <Title/> 
     </div>
+    
   );
 }
 
---
+++ src/components/MyComponents.js
@@ -0,0 +1,12 @@
+import './MyComponents.css'
+
+const MyComponents = () => {
+  return (
+    <div>
+        <h1>CSS de componente</h1>
+        <p>Parágrafo do componente</p>
+    </div>
+  )
+}
+
+export default MyComponents
